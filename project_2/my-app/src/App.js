de66ab13fd10007ad89f93f72798b3efeeffe057
searchmoon <moonding901212@gmail.com>
2022-09-16 23:53:23 +0900

    today I learned

--- project_2/my-app/src/App.js
+++ project_2/my-app/src/App.js
@@ -1,9 +1,9 @@
-import React from "react"
+import { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import UserContext from "./context/UserContext";
 import BlogDetail from "./pages/BlogDetail/BolgDetail";
 import Home from "./pages/Home/Home"
-import data from "./database/data.json"
+// import data from "./database/data.json"
 
 function App() {
     const [userId, setUserId] = useState(0)
--- project_2/my-app/src/pages/Home/Home.jsx
+++ project_2/my-app/src/pages/Home/Home.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import "./home.css"
+
 const Home = () => {
+    const [posts, setPosts] = useState()
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/posts")
+            .then((json) => {
+                setPosts(json.data)
+            })
+    }, [])
+    console.log(posts)
     return (
         <div>
             Home
+            {posts.map((post) => <div key={post.id}>{post.title}</div>
+            )}
         </div>
     );
 };
