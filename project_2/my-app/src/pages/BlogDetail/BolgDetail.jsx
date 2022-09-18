6864df81f11ad94140b72e3baabd41c4aa0d7e35
searchmoon <moonding901212@gmail.com>
2022-09-18 16:47:57 +0900

    today I learned

--- project_2/my-app/src/pages/BlogDetail/BolgDetail.jsx
+++ project_2/my-app/src/pages/BlogDetail/BolgDetail.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react"
+import { useLocation } from 'react-router-dom'
+import axios from "axios"
+
 const BolgDetail = () => {
+    const location = useLocation()
+    const postNumber = location.pathname.split('/')[2]
+
+    const [post, setPost] = useState(undefined)
+    console.log(postNumber)
+    useEffect(() => {
+        axios
+            .get(`http://localhost:3001/posts/${postNumber}`)
+            .then((json) => {
+                setPost(json.data)
+            })
+            .catch((e) => {
+                setPost(undefined)
+            })
+    }, [])
+    // console.log(post)
     return (
         <div>
-            BolgDetail
+            {post === undefined ?
+                <div>로딩중..</div> : post === null ?
+                    <div>404</div> : <div>{post.title}</div>}
         </div>
     );
 };
